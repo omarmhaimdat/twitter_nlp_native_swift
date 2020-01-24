02d84170388da9be1c265c33fabaaec098aa0784
Omar MHAIMDAT <omarmhaimdat@gmail.com>
2020-01-24 14:29:11 +0100

    First commit

--- API/app.py
+++ API/app.py
@@ -5,10 +5,10 @@ import twitter
 app = Flask(__name__)
 api = Api(app)
 
-ACCESS_TOKEN = '526136285-OABt60RomqxJvfDxQZdQwldqVnuOdAHMt4sLhESw'
-ACCESS_TOKEN_SECRET = 'TfxDxvp95uq4CHwJYiyFnIjlnj1wCm3M1Q4B62RLMm1B7'
-CONSUMER_KEY = 'KfnSHNfppfQSH0KETwyI5sg4d'
-CONSUMER_SECRET = 'Um3tbIvf3eGNghLKZ0YIw42ZhgLBsBh97ohG0HLKaotgEXo27v'
+ACCESS_TOKEN = ''
+ACCESS_TOKEN_SECRET = ''
+CONSUMER_KEY = ''
+CONSUMER_SECRET = ''
 
 twitter_api = twitter.Api(CONSUMER_KEY,
                           CONSUMER_SECRET,
